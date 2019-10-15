4835f4ac3eae759b0f7e316859221c4ec3861403
xianhe005 <xianhe005@yeah.net>
2019-10-15 17:39:45 +0800

    死锁产生与分析

--- src/main/java/com/hxh/juc/DeadLockDemo.java
+++ src/main/java/com/hxh/juc/DeadLockDemo.java
@@ -31,10 +31,29 @@ class HoldLockThread implements Runnable {
  */
 public class DeadLockDemo {
 
+    /*
+     * linux    ps -ef|grep xxxx    ls -l
+     * windows下的java运行程序 也有类似ps的查看进程的命令，但是目前我们需要查看的只是java
+     *          jps = java ps       jps -l
+     */
+
+    /**
+     * 死锁java命令分析<br/><br/>
+     * 1.通过jps -l命令，查看应用进程ID<br/>
+     * <img src="../../../../../img/jps.png" width="800" height="300" alt="jps -l" /><br/>
+     * <br/>
+     * 2.通过jstack 15056(应用进程ID)命令查看错误栈信息<br/>
+     * <img src="../../../../../img/jstack.png" width="800" height="300" alt="jstack 15056(应用进程ID)" /><br/>
+     * <br/>
+     * 3.发现死板，分析死锁<br/>
+     * <img src="../../../../../img/deadlock.png" width="800" height="300" alt="分析死锁" /><br/>
+     */
     public static void main(String[] args) {
         String lockA = "lockA";
         String lockB = "lockB";
         new Thread(new HoldLockThread(lockA, lockB), "AAA").start();
-        new Thread(new HoldLockThread(lockB, lockA), "AAA").start();
+        new Thread(new HoldLockThread(lockB, lockA), "BBB").start();
+
+
     }
 }
